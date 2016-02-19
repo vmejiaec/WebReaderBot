b8451a6f4076e5aa665058e65ef1e68f43baea25
vmejiaec <vmejiaec@gmail.com>
2016-02-18 20:11:29 -0500

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import java.io.PrintStream;
+
+/**
+ * Created by Victor on 18/02/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Muy buen IDE!");
+        int n=10;
+        n=n+1;
+        System.out.printf("Numero: "+n);
+    }
+}
